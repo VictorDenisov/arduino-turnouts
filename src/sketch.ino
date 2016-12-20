bf82a976e50367c04084e15d2fcde676af7aadb9
Victor Denisov <denisovenator@gmail.com>
2016-12-20 13:57:24 -0800

    Keep track of states of all turnouts

--- src/sketch.ino
+++ src/sketch.ino
@@ -2,22 +2,56 @@ int pinout[] = {8, 9, 10, 11};
 
 int pin_count = sizeof(pinout) / sizeof(pinout[0]);
 
+int state[] = {0, 2};
+int state_count = pin_count / 2;
+
+void apply_state() {
+	for (int i = 0; i < state_count; ++i) {
+		digitalWrite(pinout[state[i]], HIGH);
+		delay(200);
+		digitalWrite(pinout[state[i]], LOW);
+		delay(500);
+	}
+}
+
+void report_state() {
+	char *st = "-----a---c-----";
+	if (state[0] == 0) {
+		st[2] = '-';
+	}
+	if (state[0] == 1) {
+		st[2] = '/';
+	}
+	if (state[1] == 2) {
+		st[12] = '-';
+	}
+	if (state[1] == 3) {
+		st[12] = '\\';
+	}
+	Serial.println("    -b---d-    ");
+	Serial.println("   /       \\   ");
+	Serial.println(st);
+}
+
 void setup()
 {
 	for (int i = 0; i < pin_count; ++i) {
 		pinMode(pinout[i], OUTPUT);
 	}
 
+	apply_state();
+
 	Serial.begin(9600);
+	report_state();
 }
 
 void serialEvent() {
 	int incomingByte = Serial.read();
 	int pinNum = incomingByte - 'a';
-	if (pinNum > 0 && pinNum < pin_count) {
-		digitalWrite(pinout[pinNum], HIGH);
-		delay(200);
-		digitalWrite(pinout[pinNum], LOW);
+	if (pinNum >= 0 && pinNum < pin_count) {
+		state[pinNum >> 1] = pinNum;
+		apply_state();
+		report_state();
 	}
 }
 

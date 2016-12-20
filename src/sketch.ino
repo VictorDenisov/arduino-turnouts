acfb05ad88d02fba3ff22502163dd6a6c9977ab7
Victor Denisov <denisovenator@gmail.com>
2016-12-20 13:21:51 -0800

    Simplify current code

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,42 +1,28 @@
+int pinout[] = {8, 9, 10, 11};
+
+int pin_count = sizeof(pinout) / sizeof(pinout[0]);
 
 void setup()
 {
+	for (int i = 0; i < pin_count; ++i) {
+		pinMode(pinout[i], OUTPUT);
+	}
 
-	pinMode(8, OUTPUT);
-	pinMode(9, OUTPUT);
-	pinMode(10, OUTPUT);
-	pinMode(11, OUTPUT);
 	Serial.begin(9600);
 }
 
 void serialEvent() {
 	int incomingByte = Serial.read();
-	if (incomingByte == 'a') {
-		Serial.write('a');
-		digitalWrite(8, HIGH);
+	int pinNum = incomingByte - 'a';
+	if (pinNum > 0 && pinNum < pin_count) {
+		digitalWrite(pinout[pinNum], HIGH);
 		delay(200);
-		digitalWrite(8, LOW);
-	}
-	if (incomingByte == 'b') {
-		Serial.write('b');
-		digitalWrite(9, HIGH);
-		delay(200);
-		digitalWrite(9, LOW);
-	}
-	if (incomingByte == 'c') {
-		Serial.write('c');
-		digitalWrite(10, HIGH);
-		delay(200);
-		digitalWrite(10, LOW);
-	}
-	if (incomingByte == 'd') {
-		Serial.write('d');
-		digitalWrite(11, HIGH);
-		delay(200);
-		digitalWrite(11, LOW);
+		digitalWrite(pinout[pinNum], LOW);
 	}
 }
 
 void loop()
 {
 }
+
+// vim: ai cin
